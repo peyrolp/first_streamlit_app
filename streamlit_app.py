cd5951572043d9816366d9d0ba46bd8fa8f82762
peyrolp <82752020+peyrolp@users.noreply.github.com>
2023-03-20 21:57:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 
 # dessous, ca normalise le texte qui est dans la reponse fruityvice
