19b30f973558a95595ed5664f6e4b31b5bf4e96a
peyrolp <82752020+peyrolp@users.noreply.github.com>
2023-03-21 22:24:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.dataframe(fruits_to_show)
 # 21/03/2023
 # nouvelle section avec fruityvice
 # creation d'une fonction pour afficher les infos venant de fruityvice
-def get_fruitvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
